da54440b6e756fe63906c59446539b34cd981dcd
yujie.fan <scwfyj@163.com>
2017-05-14 15:19:01 +0800

    Scanner test

---
+++ Chapter03/InputTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class InputTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner  in = new Scanner(System.in);
+		
+		System.out.println("what is your name");
+		String name = in.nextLine();
+		
+		System.out.println("How old are you?");
+		int age = in.nextInt();
+		
+		System.out.println("hello , "+ name + ", next year , you will be " + (age+1));
+		in.close();
+
+	}
+
+}
